a8030f877875b086f840c4adcc38034541932c85
novomilionario477-prog <novomilionario477@gmail.com>
2026-09-18 16:37:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "online",
+        nome: "Minha IA"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Minha IA rodando na porta ${PORT}`);
+});
